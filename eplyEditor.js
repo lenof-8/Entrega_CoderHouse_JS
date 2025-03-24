bf30512cc7f0f6c0399869589dad08685660e951
lenof <lenof_@outlook.com>
2025-03-24 00:22:56 -0500

    Avances en el editor, NO está terminado, falta la modificación correcta de horarios no habilitados, la muestra de datos y el reemplazar los datos del almacenamiento

--- eplyEditor.js
+++ eplyEditor.js
@@ -4,15 +4,18 @@ const employeesList = [];
 
 const errorMessage = document.getElementById('errorMessage');
 const editorDiv = document.getElementById('editorDiv')
-
+const weekdays = ['Lunes', 'Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'];
 function openEmpEditorMenu(employee = new Employee )
 {
+    console.log(employee)
+
     editorDiv.innerHTML = "";
 
     const name_Div = document.createElement('div');
     const hrsWkly_Div = document.createElement('div');
     const hrsDbt_Div = document.createElement('div');
     const unavlb_Div = document.createElement('div');
+    const unavlbShow_Div = document.createElement('div');
     
 
     name_Div.appendChild(Object.assign(document.createElement('label'), { innerText: "Nombre del empleado: "}))
@@ -35,6 +38,67 @@ function openEmpEditorMenu(employee = new Employee )
     hrsDbt_Inpt.setAttribute("type", 'number');
     
     editorDiv.append(name_Div, hrsWkly_Div, hrsDbt_Div, unavlb_Div);
+
+    let unavlbSch = employee.getHoursUnavailable();
+
+    console.log(unavlbSch)
+    const unavlb_Slct = document.createElement('select', { placeHolder: 'Horario' }) ;
+    unavlb_Div.appendChild(Object.assign(document.createElement('label'), {innerText: 'Horas no disponibles: '}))
+    unavlb_Div.appendChild(unavlb_Slct);
+    const unavlbMenu = document.createElement('div');
+    unavlbMenu.appendChild(Object.assign(document.createElement('label'), { innerText: 'Hora de inicio: '}));
+    const hrs1_Inpt = unavlbMenu.appendChild(Object.assign(document.createElement('input'), { placeholder: 'HH:MM'}));
+    unavlbMenu.appendChild(Object.assign(document.createElement('label'), { innerText: ' Hora de fin: '}));
+    const hrs2_Inpt = unavlbMenu.appendChild(Object.assign(document.createElement('input'), { placeholder: 'HH:MM'}));
+    unavlbMenu.appendChild(Object.assign(document.createElement('label'), { innerText: ' Día: '}));
+    const day_Slct = unavlbMenu.appendChild(Object.assign(document.createElement('select')));
+    day_Slct.appendChild(Object.assign(document.createElement('option'), { disabled: true, selected: true, innerText: 'Día ' }))
+    weekdays.forEach(wDay => {
+        const wDay_Opt = document.createElement('option');
+        wDay_Opt.value = (wDay.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase());
+        wDay_Opt.innerText = wDay;
+        day_Slct.appendChild(wDay_Opt);
+    })
+
+
+    unavlb_Slct.appendChild(Object.assign(document.createElement('option'), { disabled: true, selected: true, innerText: 'Horario a editar' }))
+    unavlb_Div.appendChild(unavlbMenu);
+
+    unavlbSch.forEach(sch => {
+        if(sch.some(e => e !== undefined))
+        {    
+            const hrs = sch[0];
+            const hrs1 = sch[0][0];
+            const hrs2 = sch[0][1];
+            const day = sch[1];
+
+            const hrsTxt = sch[0].join(' - ');
+            const unavlb_Opt = unavlb_Slct.appendChild(Object.assign(document.createElement('option'), { value: sch, innerText: `${hrsTxt}, ${day}`}));
+            
+            unavlb_Slct.addEventListener('change', () =>{
+                hrs1_Inpt.value = null;
+                hrs2_Inpt.innerText = null;
+                if(unavlb_Slct.value == sch)
+                {
+                    hrs1_Inpt.setAttribute('placeholder', hrs1);
+                    hrs2_Inpt.setAttribute('placeholder', hrs2);
+                    day_Slct.value = day;
+
+                    hrs1_Inpt.addEventListener('change', () =>{
+                        sch[0][0] = hrs1_Inpt.value;
+                        console.log(unavlbSch)
+                    })
+
+                    hrs2_Inpt.addEventListener('change', () =>{
+                        sch[0][1] = hrs2_Inpt.value;
+                        console.log(unavlbSch)
+                    })
+                }
+            })
+        }
+    })
+    
+
 }
 
 
